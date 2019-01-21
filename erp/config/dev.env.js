b75a1197927880488ac8459a11d11b3acfd38355
Zs <1582293282@qq.com>
2019-01-21 22:47:06 +0800

    z

--- erp/config/dev.env.js
+++ erp/config/dev.env.js
@@ -4,6 +4,7 @@ const prodEnv = require('./prod.env');
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_ROOT_MAIN: '"http://192.168.10.211:8095"'
+  // API_ROOT_MAIN: '"http://192.168.10.211:8095"'
+  API_ROOT_MAIN: '"http://www.chinayie.com:8095"'
 });
 
