e7b27d9976f3038c227b89d4826c691cff4bc809
zhangshuai <zhangshuai@chinayie.com>
2019-02-25 13:44:18 +0800

    Revert "z"
    This reverts commit 8fa0f9342ee6760b51b71560b6c8a4832e5d458b.

--- helloWord/src/router/index.js
+++ helloWord/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import tuodong from '@/components/tuodong'
+import flexBox from '@/components/flexbox'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/tuodong',
       name: 'tuodong',
       component: tuodong
+    },
+    {
+      path: '/flexBox',
+      name: 'flexBox',
+      component: flexBox
     }
   ]
 })
